7a1c5e6ecfb9049101734acb41b2829c47308a72
Illia Avdeienko <Whiter4112@gmail.com>
2022-09-01 04:44:39 +0300

    Create enemy and set up enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,105 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Character
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum State
     {
-        
+        Idle,
+        Chase,
+        Attack
     }
 
-    // Update is called once per frame
-    void Update()
+    private State curState = State.Idle;
+
+    [Header("Ranges")]
+    [SerializeField] private float chaseRange;
+    [SerializeField] private float attackRange;
+
+    [Header("Attack")]
+    [SerializeField] private float attackRate;
+    [SerializeField] private GameObject attackPrefab;
+    private float _lastAttackTime;
+
+    private float _targetDistance;
+
+    private void Start()
+    {
+        Target = Player.Current;
+    }
+
+    private void Update()
+    {
+        if(Target==null)
+            return;
+        
+        _targetDistance = Vector3.Distance(transform.position, Target.transform.position);
+        
+        switch (curState)
+        {
+            case State.Idle:
+                IdleUpdate();
+                break;
+            case State.Attack:
+                AttackUpdate();
+                break;
+            case State.Chase:
+                ChaseUpdate();
+                break;
+        }
+    }
+
+    void SetState(State newState)
+    {
+        curState = newState;
+
+        switch (curState)
+        {
+            case State.Idle:
+                controller.StopMovement();
+                break;
+            case State.Chase:
+                controller.MoveToTarget(Target.transform);
+                break;
+            case State.Attack:
+                controller.StopMovement();
+                break;
+        }
+    }
+
+    void IdleUpdate()
+    {
+        if(_targetDistance <chaseRange && _targetDistance>attackRange)
+            SetState(State.Chase);
+        else if (_targetDistance<attackRange)
+            SetState(State.Attack);
+    }
+
+    void ChaseUpdate()
+    {
+        if(_targetDistance>chaseRange)
+            SetState(State.Idle);
+        else if(_targetDistance < attackRange)
+            SetState(State.Attack);
+    }
+
+    void AttackUpdate()
+    {
+        if(_targetDistance>attackRange)
+            SetState(State.Chase);
+        
+        controller.LookTowards(Target.transform.position - transform.position);
+
+        if (Time.time - _lastAttackTime > attackRate)
+        {
+            _lastAttackTime = Time.time;
+            AttackTarget();
+        }
+    }
+
+    void AttackTarget()
     {
         
     }
