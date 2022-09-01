da5ab52137226785246e4f6da00ba644707cda59
Illia Avdeienko <Whiter4112@gmail.com>
2022-09-01 05:27:31 +0300

    Added health bars for player and enemy and set up script for it

--- Assets/Scripts/HealthBarUI.cs
+++ Assets/Scripts/HealthBarUI.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image healthFill;
+    [SerializeField] private Character character;
+
+    private void OnEnable()
     {
-        
+        character.onTakeDamage += UpdateHealthBar;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        character.onTakeDamage -= UpdateHealthBar;
+    }
+
+    private void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+    }
+
+    void UpdateHealthBar()
+    {
+        healthFill.fillAmount = (float)character.curHp / (float)character.maxHp;
     }
 }
