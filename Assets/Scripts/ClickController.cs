751ed5827355d0c1bc55421637994abf8c3d6f3a
Illia Avdeienko <Whiter4112@gmail.com>
2022-09-01 03:47:52 +0300

    Set up ClickController script

--- Assets/Scripts/ClickController.cs
+++ Assets/Scripts/ClickController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClickController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private LayerMask layerMask;
+
+    private void Update()
     {
-        
+        if(Mouse.current.rightButton.wasPressedThisFrame)
+            Click();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Click()
     {
-        
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            int hitLayer = hit.collider.gameObject.layer;
+
+            if (hitLayer == LayerMask.NameToLayer("Ground"))
+            {
+                Player.Current.SetTarget(null);
+                Player.Current.controller.MoveToPosition(hit.point);
+            }
+            else if (hitLayer == LayerMask.NameToLayer("Enemy"))
+            {
+                Character enemy = hit.collider.GetComponent<Character>();
+                Player.Current.SetTarget(enemy);
+            }
+        }
     }
 }
